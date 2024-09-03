7020ce71ccfe514b54f722a4132d0ff872c671f0
guox15 <guox15@ziroom.com>
2024-09-03 10:46:40 +0800

    123

--- ytl-crm-service/src/main/java/com/ytl/crm/domain/resp/work/WorkWechatExternalUserIdResp.java
+++ ytl-crm-service/src/main/java/com/ytl/crm/domain/resp/work/WorkWechatExternalUserIdResp.java
@@ -1,7 +1,6 @@
 package com.ytl.crm.domain.resp.work;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.ziroom.wechat.service.domain.constant.Constants;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
