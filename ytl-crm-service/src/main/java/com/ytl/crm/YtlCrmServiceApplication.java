0d58fe5f791500bf127ac6725b80ac3bf1be17fb
guox15 <guox15@ziroom.com>
2024-09-09 13:10:26 +0800

    12

--- ytl-crm-service/src/main/java/com/ytl/crm/YtlCrmServiceApplication.java
+++ ytl-crm-service/src/main/java/com/ytl/crm/YtlCrmServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 //@EnableScheduling
 //@EnableAsync
-//@Import(DruidDataSourceConfig.class)
 @EnableCaching
 @EnableFeignClients
 @MapperScan(basePackages = {"com.ytl.crm.mapper"})
