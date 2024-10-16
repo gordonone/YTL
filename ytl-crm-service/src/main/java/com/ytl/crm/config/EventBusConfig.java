fb45b2459e613f73a1040666e5450ea3dd1480ce
guox15 <guox15@ziroom.com>
2024-10-16 21:27:30 +0800

    修改路径

---
+++ ytl-crm-service/src/main/java/com/ytl/crm/config/EventBusConfig.java
@@ -0,0 +1,50 @@
+package com.ytl.crm.config;
+
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
+import com.ytl.crm.event.wechat.listener.IWeChatEventListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Configuration
+public class EventBusConfig {
+
+    @Resource
+    private List<IWeChatEventListener> weChatEventListenerList;
+
+    @Bean("weChatEventBus")
+    public EventBus eventBus() {
+        EventBus eventBus = new AsyncEventBus("weChatEventBus", threadPoolExecutor());
+        if (!CollectionUtils.isEmpty(weChatEventListenerList)) {
+            for (IWeChatEventListener listener : weChatEventListenerList) {
+                eventBus.register(listener);
+            }
+        }
+        return eventBus;
+    }
+
+    @Bean("weChatEventBusThreadPool")
+    public ThreadPoolExecutor threadPoolExecutor() {
+
+        return new ThreadPoolExecutor(5, 5, 60, TimeUnit.SECONDS, new ArrayBlockingQueue<>(500));
+//        return ThreadPoolExecutor.(
+//                "weChatEventBusThreadPool",
+//                Runtime.getRuntime().availableProcessors() * 2,
+//                Runtime.getRuntime().availableProcessors() * 10,
+//                60,
+//                TimeUnit.SECONDS,
+//                new ArrayBlockingQueue<>(500),
+//                new ThreadPoolExecutor.CallerRunsPolicy());
+    }
+
+}
