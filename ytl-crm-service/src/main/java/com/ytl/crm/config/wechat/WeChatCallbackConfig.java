06a0fe296b888dbb7aa244fd95e556ab304f8d86
guox15 <guox15@ziroom.com>
2024-10-16 21:22:55 +0800

    修改路径

--- ytl-crm-service/src/main/java/com/ytl/crm/config/wechat/WeChatCallbackConfig.java
+++ ytl-crm-service/src/main/java/com/ytl/crm/config/wechat/WeChatCallbackConfig.java
@@ -14,10 +14,10 @@ public class WeChatCallbackConfig {
     @Value("${weChat.official.callback.corpId:}")
     private String officialCallBackCorpId;
 
-    @Value("${weChat.official.callback.token:}")
+    @Value("${weChat.official.callback.token:dNnDKuTUwWR}")
     private String officialCallBackToken;
 
-    @Value("${weChat.official.callback.aseKeyBase64:}")
+    @Value("${weChat.official.callback.aseKeyBase64:VUdDLUNSTS1aSVJPT00tV0VDSEFULUFCQyFAIyRYWlk}")
     private String officialCallBackAesKeyBase6;
 
     //@ApolloJsonValue("${weChat.official.callback.officialSupportEventMap:{}}")
