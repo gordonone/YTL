f39bdc71c6bbc35aa57d97b5ef42cbf582d594c2
guox15 <guox15@ziroom.com>
2024-10-29 14:05:22 +0800

    提交素材

---
+++ ytl-crm-service/src/main/java/com/ytl/crm/api/callback/wechat/WechatAuthApi.java
@@ -0,0 +1,15 @@
+package com.ytl.crm.api.callback.wechat;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WechatAuthApi {
+
+    @RequestMapping("/*")
+    public String getCheckInfo() {
+        return "b9To9JT945BLlfF5";
+    }
+
+}
+
