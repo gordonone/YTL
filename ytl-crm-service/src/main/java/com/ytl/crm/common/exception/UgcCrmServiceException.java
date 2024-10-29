9e8ecd9e6103a57ebb900a7cecd22c3613c8987b
guox15 <guox15@ziroom.com>
2024-10-29 13:48:09 +0800

    提交素材

--- ytl-crm-service/src/main/java/com/ytl/crm/common/exception/UgcCrmServiceException.java
+++ ytl-crm-service/src/main/java/com/ytl/crm/common/exception/UgcCrmServiceException.java
@@ -1,16 +1,15 @@
 package com.ytl.crm.common.exception;
 
 
-import com.ytl.crm.common.base.UgcCmrServiceRespCodeEnum;
 import com.ytl.crm.domain.common.BaseException;
 
 
 public class UgcCrmServiceException extends BaseException {
 
-    public static final int BIZ_ERROR_CODE = 999000001;
+    public static int BUSINESS_CODE = 10001;
 
     public UgcCrmServiceException(String message, Throwable cause) {
-        super(BIZ_ERROR_CODE, message, cause);
+        super(BUSINESS_CODE, message, cause);
     }
 
     public UgcCrmServiceException(int code, String message, Throwable cause) {
@@ -21,16 +20,8 @@ public class UgcCrmServiceException extends BaseException {
         super(code, message);
     }
 
-    public UgcCrmServiceException(UgcCmrServiceRespCodeEnum errorEnum) {
-        super(errorEnum.getCode(), errorEnum.getMsg());
-    }
-
     public UgcCrmServiceException(String message) {
-        super(BIZ_ERROR_CODE, message);
-    }
-
-    public UgcCrmServiceException(Throwable cause) {
-        super(cause);
+        super(BUSINESS_CODE, message);
     }
 
 }
