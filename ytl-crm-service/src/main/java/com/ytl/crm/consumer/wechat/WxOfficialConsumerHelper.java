0adaad53a0f70134e7941ccac87e254490c60df8
guox15 <guox15@ziroom.com>
2024-11-01 13:12:12 +0800

    提交素材

--- ytl-crm-service/src/main/java/com/ytl/crm/consumer/wechat/WxOfficialConsumerHelper.java
+++ ytl-crm-service/src/main/java/com/ytl/crm/consumer/wechat/WxOfficialConsumerHelper.java
@@ -10,6 +10,7 @@ import com.ytl.crm.consumer.req.wechat.WechatDeleteQrCodeReq;
 import com.ytl.crm.consumer.resp.wechat.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.AnnotationUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
@@ -35,76 +36,82 @@ public class WxOfficialConsumerHelper {
     private static final String GET_TMP_MATERIAL_MEDIA_URL = "https://qyapi.weixin.qq.com/cgi-bin/media/get?access_token=ACCESS_TOKEN&media_id=MEDIA_ID";
 
 
-    public String uploadFile(MultipartFile file, String fileType) throws Exception {
-
-
-        String accessToken = wxOfficialTokenHelper.acquireAccessToken();
+    public String uploadFile(MultipartFile file, String fileType) {
 
         String result = null;
-        String url = "https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=+" + accessToken + "&type=" + fileType;
-        /**
-         * 第一部分
-         */
-        URL urlObj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
-        con.setRequestMethod("POST"); // 以Post方式提交表单，默认get方式
-        con.setDoInput(true);
-        con.setDoOutput(true);
-        con.setUseCaches(false); // post方式不能使用缓存
-        // 设置请求头信息
-        con.setRequestProperty("Connection", "Keep-Alive");
-        con.setRequestProperty("Charset", "UTF-8");
-        // 设置边界
-        String BOUNDARY = "----------" + System.currentTimeMillis();
-        con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
-        // 请求正文信息
-        // 第一部分：
-        StringBuilder sb = new StringBuilder();
-        sb.append("--"); // 必须多两道线
-        sb.append(BOUNDARY);
-        sb.append("\r\n");
-        sb.append("Content-Disposition: form-data;name=\"media\";filename=\"" + file.getOriginalFilename() + "\"\r\n");
-        sb.append("Content-Type:application/octet-stream\r\n\r\n");
-        byte[] head = sb.toString().getBytes("utf-8");
-        // 获得输出流
-        OutputStream out = new DataOutputStream(con.getOutputStream());
-        // 输出表头
-        out.write(head);
-        // 文件正文部分
-        // 把文件已流文件的方式 推入到url中
-        DataInputStream in = new DataInputStream(file.getInputStream());
-        int bytes = 0;
-        byte[] bufferOut = new byte[1024];
-        while ((bytes = in.read(bufferOut)) != -1) {
-            out.write(bufferOut, 0, bytes);
-        }
-        in.close();
-        // 结尾部分
-        byte[] foot = ("\r\n--" + BOUNDARY + "--\r\n").getBytes("utf-8");// 定义最后数据分隔线
-        out.write(foot);
-        out.flush();
-        out.close();
 
-        StringBuffer buffer = new StringBuffer();
-        BufferedReader reader = null;
         try {
-            // 定义BufferedReader输入流来读取URL的响应
-            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                buffer.append(line);
+            String accessToken = wxOfficialTokenHelper.acquireAccessToken();
+            String url = "https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=+" + accessToken + "&type=" + fileType;
+            /**
+             * 第一部分
+             */
+            URL urlObj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+            con.setRequestMethod("POST"); // 以Post方式提交表单，默认get方式
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setUseCaches(false); // post方式不能使用缓存
+            // 设置请求头信息
+            con.setRequestProperty("Connection", "Keep-Alive");
+            con.setRequestProperty("Charset", "UTF-8");
+            // 设置边界
+            String BOUNDARY = "----------" + System.currentTimeMillis();
+            con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+            // 请求正文信息
+            // 第一部分：
+            StringBuilder sb = new StringBuilder();
+            sb.append("--"); // 必须多两道线
+            sb.append(BOUNDARY);
+            sb.append("\r\n");
+            sb.append("Content-Disposition: form-data;name=\"media\";filename=\"" + file.getOriginalFilename() + "\"\r\n");
+            sb.append("Content-Type:application/octet-stream\r\n\r\n");
+            byte[] head = sb.toString().getBytes("utf-8");
+            // 获得输出流
+            OutputStream out = new DataOutputStream(con.getOutputStream());
+            // 输出表头
+            out.write(head);
+            // 文件正文部分
+            // 把文件已流文件的方式 推入到url中
+            DataInputStream in = new DataInputStream(file.getInputStream());
+            int bytes = 0;
+            byte[] bufferOut = new byte[1024];
+            while ((bytes = in.read(bufferOut)) != -1) {
+                out.write(bufferOut, 0, bytes);
             }
-            if (result == null) {
+            in.close();
+            // 结尾部分
+            byte[] foot = ("\r\n--" + BOUNDARY + "--\r\n").getBytes("utf-8");// 定义最后数据分隔线
+            out.write(foot);
+            out.flush();
+            out.close();
+
+            StringBuffer buffer = new StringBuffer();
+            BufferedReader reader = null;
+            try {
+                // 定义BufferedReader输入流来读取URL的响应
+                reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
                 result = buffer.toString();
+
+            } catch (IOException e) {
+                log.info("发送POST请求出现异常！ {}", e);
+                e.printStackTrace();
+                // throw new IOException("数据读取异常");
+            } finally {
+                if (reader != null) {
+                    reader.close();
+                }
             }
-        } catch (IOException e) {
-            log.info("发送POST请求出现异常！ {}", e);
+        } catch (Exception e) {
             e.printStackTrace();
-            throw new IOException("数据读取异常");
-        } finally {
-            if (reader != null) {
-                reader.close();
-            }
+        }
+
+        if (StringUtils.isBlank(result)) {
+            throw new UgcCrmServiceException("临时素材上传失败！");
         }
 
         JSONObject jsonObj = JSON.parseObject(result);
