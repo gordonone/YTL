b38d18ba865c56528f02c113b6360f682490bdbf
guox15 <guox15@ziroom.com>
2024-11-01 18:12:27 +0800

    提交素材

--- ytl-crm-service/src/main/java/com/ytl/crm/config/MybatisPlusConfig.java
+++ ytl-crm-service/src/main/java/com/ytl/crm/config/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.ytl.crm.config;
 
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
@@ -21,6 +22,13 @@ public class MybatisPlusConfig {
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
         // 如果有多数据源可以不配具体类型, 否则都建议配上具体的 DbType
         return interceptor;
+    }
+
+    @Bean
+    public GlobalConfig globalConfig() {
+        GlobalConfig config = new GlobalConfig();
+        config.setMetaObjectHandler(new MyMetaObjectHandler());
+        return config;
     }
 }
 
