31b5ea896dce002dd30a03080208d0a7b1f37b8c
guox15 <guox15@ziroom.com>
2024-11-01 18:11:15 +0800

    提交素材

---
+++ ytl-crm-service/src/main/java/com/ytl/crm/config/MyMetaObjectHandler.java
@@ -0,0 +1,20 @@
+package com.ytl.crm.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "logicCode", String.class, "defaultLogicCode");
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+
+    }
+}
